4e5f928c0421633abdd4a21dd2e3258146dde70c
Kevin Nadro <sodapopcuffgames@gmail.com>
2015-12-20 00:49:29 -0600

    all broken

--- btree.js
+++ btree.js
@@ -24,35 +24,12 @@ function B_TREE(m){
 	this.root = null;
 }
 
-
 /*
-	B-TREE constructor
-*/ 
-B_TREE.prototype.init = function(){
-
-}
-
-/* 
-	Search
-*/
-B_TREE.prototype.search = function(){
-
-}
-
-
-/* 
 	Insertion
 	Args:
 		value: the number to be inserted
 */
 B_TREE.prototype.insert = function(value){
-
-	if(this.root === null){
-		this.root = new B_TREE_NODE();
-		this.root.insert(value);
-		this.root.order = this.m;
-	}
-
 }
 
 /*
--- btreeNode.js
+++ btreeNode.js
@@ -2,39 +2,19 @@
 	CS 225 APPLET B-TREE 
 */
 
-function B_TREE_NODE(){
+function B_TREE_NODE(m,leaf){
 	/* An array of keys */
 	this.keys = [];
+	
 	/* Current number of keys */
 	this.numberOfKeys = 0;
+
 	/* Am I a leaf */
-	this.leaf = 0;
+	this.leaf = leaf;
 
-	/* Max keys */
-	this.order = 0;
-}
+	/* Determines max keys */
+	this.m = m;
 
-
-/*
-	Constructor
-*/
-B_TREE_NODE.prototype.init = function(){
-
-}
-
-
-/*
-	Insert
-		Inserts the value into the keys array, in order
-
-	Call .sort(function(a,b){return a-b}); for now on keys
-*/
-B_TREE_NODE.prototype.insert = function(value){
-	/* Insert value into the array */
-	this.keys.push(value);
-	/* Sort the keys */
-	this.keys.sort(function(a,b){return a-b});
-
-	/* Increment the number of keys */
-	this.numberOfKeys++;
+	/* Child nodes */
+	this.children = [];
 }
