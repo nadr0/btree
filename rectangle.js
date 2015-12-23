460142c2c91f5e69e4d558382ba6050e01d01349
Kevin Nadro <sodapopcuffgames@gmail.com>
2015-12-23 10:19:18 -0600

    rectangle

---
+++ rectangle.js
@@ -0,0 +1,11 @@
+function rectangleBoundary(left,right,top,bottom,spacing){
+
+	this.left = left;
+	this.right = right;
+	this.top = top;
+	this.bottom = bottom;
+
+	this.spacing = spacing;
+
+	this.nodes = [];
+}
